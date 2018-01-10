ba23c3c0381f4e810248cd7bfc80e43ecf44642c
Varonss <martin.varona.84@gmail.com>
2018-01-09 20:17:28 -0600

    "Finish exercise 7"

--- src/recursion.js
+++ src/recursion.js
@@ -83,8 +83,16 @@ var range = function(x, y) {
 // The exponent of a number says how many times the base number is used as a factor.
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
 // exponent(4,3); // 64
-// https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
+  // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp < 0)
+   return exponent(base, exp + 1)/base
+  if(exp === 0)
+  return 1;
+   else
+  return base * exponent(base, exp - 1)
+
+
 };
 
 // 8. Determine if a number is a power of two.
